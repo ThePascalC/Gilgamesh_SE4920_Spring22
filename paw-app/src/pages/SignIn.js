fc4b9ffcbb4f6f6a041e0c998c0c287c1e86f06d
Aaron7Thole <aaronr2017@gmail.com>
2022-03-23 08:57:09 -0500

    New Login Page
    Cleaned up and added color to the Login page

--- paw-app/src/pages/SignIn.js
+++ paw-app/src/pages/SignIn.js
@@ -1,48 +1,51 @@
 import "./CssPages/SignIn.css";
 import React, { Component, useState } from 'react';
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-
+import Googlelogo from '../path/google.png';
+import AppleLogo from '../path/apple.png';
+import FaceLogo from '../path/facebook.png';
 export default function LoginFunction() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     function validateForm() {
-    	return email.length > 0 && password.length > 0;
+        return email.length > 0 && password.length > 0;
     }
-        return (
-            <form>
-                <div className='form-group'>
-                    <div className='layer-group'>
-                        <div className='label'>
-                            <h3><span class='login'>Login</span> or <span class='signup'>Signup</span></h3>
+    return (
+        <form>
+            <div className='form-group'>
+                <div className='layer-group'>
+                    <div className='label'>
+                        <h3><span class='login'>Login to Pawsible</span></h3>
+                    </div>
+                    <div className='button-Google' >
+                        <button type="submit" className="app-signIn"><img src={Googlelogo} className='googleLogo' />Sign In with Google</button>
+                    </div>
+                    <div className='button-Apple'>
+                        <button type="submit" className="app-signIn"><img src={AppleLogo} className='appleLogo' />Sign In with Facebook</button>
                         </div>
+                    <div className='button-Facebook'>
+                        <button type="submit" className="app-signIn"><img src={FaceLogo} className='faceLogo'/>Sign In with Apple</button>
+                    </div>
+                    <h1 className="or-line">Or</h1>
+                    <div className="email-group">
+                        <label><i class='icon-us'><span class='email-label'></span></i></label>
+                        <input type="email" className="email-control" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
+                    </div>
 
-                        <div className="email-group">
-                            <label><i class='icon-us'><span class='email-label'>Email</span></i></label>
-                            <input type="email" className="email-control" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
-                        </div>
+                    <div className="password-group">
+                        <label><i class='icon-lock'><span class='password-label'></span></i></label>
+                        <input type="password" className="password-control" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
+                    </div>
 
-                        <div className="password-group">
-                            <label><i class='icon-lock'><span class='password-label'>Password</span></i></label>
-                            <input type="password" className="password-control" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
-                        </div>
-
-                        <div className="checkbox-group">
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                            </div>
-                        </div>
-
-                        <div className='submit-group'>
-                            <button type="submit" className="SignInBtn" disabled={!validateForm()}>Sign in</button>
-                            <p className="forgot-password text-right">
-                                Forgot <a href="#">password?</a>
-                            </p>
-                        </div>
+                    <div className='submit-group'>
+                        <button type="submit" className="SignInBtn" disabled={!validateForm()}>Sign in</button>
+                        <p className="forgot-password text-right">
+                            Forgot <a href="#">password?</a>
+                        </p>
                     </div>
                 </div>
-            </form>
-        );
-    }
+            </div>
+        </form>
+    );
+}
