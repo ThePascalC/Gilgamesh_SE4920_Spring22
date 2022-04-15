c8b27a324fb5b7e47a05fb8cce49b720bbd000e8
Nick Cain <45472016+ThePascalC@users.noreply.github.com>
2022-04-14 23:28:25 -0500

    Fixed Sign In Page

--- paw-app/src/pages/SignIn.js
+++ paw-app/src/pages/SignIn.js
@@ -13,45 +13,16 @@ const required = value => {
     );
   }
 };
-const email = value => {
-  if (!isEmail(value)) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This is not a valid email.
-      </div>
-    );
-  }
-};
-const vusername = value => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The username must be between 3 and 20 characters.
-      </div>
-    );
-  }
-};
-const vpassword = value => {
-  if (value.length < 6 || value.length > 40) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
-      </div>
-    );
-  }
-};
-export default class Register extends Component {
+export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.handleRegister = this.handleRegister.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
     this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeEmail = this.onChangeEmail.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
     this.state = {
       username: "",
-      email: "",
       password: "",
-      successful: false,
+      loading: false,
       message: ""
     };
   }
@@ -60,34 +31,23 @@ export default class Register extends Component {
       username: e.target.value
     });
   }
-  onChangeEmail(e) {
-    this.setState({
-      email: e.target.value
-    });
-  }
   onChangePassword(e) {
     this.setState({
       password: e.target.value
     });
   }
-  handleRegister(e) {
+  handleLogin(e) {
     e.preventDefault();
     this.setState({
       message: "",
-      successful: false
+      loading: true
     });
     this.form.validateAll();
     if (this.checkBtn.context._errors.length === 0) {
-      AuthService.register(
-        this.state.username,
-        this.state.email,
-        this.state.password
-      ).then(
-        response => {
-          this.setState({
-            message: response.data.message,
-            successful: true
-          });
+      AuthService.login(this.state.username, this.state.password).then(
+        () => {
+          this.props.history.push("/");
+          window.location.reload();
         },
         error => {
           const resMessage =
@@ -97,11 +57,15 @@ export default class Register extends Component {
             error.message ||
             error.toString();
           this.setState({
-            successful: false,
+            loading: false,
             message: resMessage
           });
         }
       );
+    } else {
+      this.setState({
+        loading: false
+      });
     }
   }
   render() {
@@ -109,61 +73,47 @@ export default class Register extends Component {
       <div className="col-md-12">
         <div className="card card-container">
           <Form
-            onSubmit={this.handleRegister}
+            onSubmit={this.handleLogin}
             ref={c => {
               this.form = c;
             }}
           >
-            {!this.state.successful && (
-              <div>
-                <div className="form-group">
-                  <label htmlFor="username">Username</label>
-                  <Input
-                    type="text"
-                    className="form-control"
-                    name="username"
-                    value={this.state.username}
-                    onChange={this.onChangeUsername}
-                    validations={[required, vusername]}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email">Email</label>
-                  <Input
-                    type="text"
-                    className="form-control"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.onChangeEmail}
-                    validations={[required, email]}
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <Input
-                    type="password"
-                    className="form-control"
-                    name="password"
-                    value={this.state.password}
-                    onChange={this.onChangePassword}
-                    validations={[required, vpassword]}
-                  />
-                </div>
-                <div className="form-group">
-                  <button className="btn btn-primary btn-block">Sign Up</button>
-                </div>
-              </div>
-            )}
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <Input
+                type="text"
+                className="form-control"
+                name="username"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+                validations={[required]}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Input
+                type="password"
+                className="form-control"
+                name="password"
+                value={this.state.password}
+                onChange={this.onChangePassword}
+                validations={[required]}
+              />
+            </div>
+            <div className="form-group">
+              <button
+                className="btn btn-primary btn-block"
+                disabled={this.state.loading}
+              >
+                {this.state.loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Login</span>
+              </button>
+            </div>
             {this.state.message && (
               <div className="form-group">
-                <div
-                  className={
-                    this.state.successful
-                      ? "alert alert-success"
-                      : "alert alert-danger"
-                  }
-                  role="alert"
-                >
+                <div className="alert alert-danger" role="alert">
                   {this.state.message}
                 </div>
               </div>
