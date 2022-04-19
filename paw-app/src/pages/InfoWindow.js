29f9ecf3e86a97067845f2d25ac1925f07e4cc3c
Aaron Thole <aaronr2017@gmail.com>
2022-04-18 19:05:15 -0500

    Popup

--- paw-app/src/pages/InfoWindow.js
+++ paw-app/src/pages/InfoWindow.js
@@ -1,24 +1,40 @@
 import * as React from 'react';
 import './CssPages/InfoWindow.css';
+import { useEffect, useState } from "react";
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import Favorite from "@material-ui/icons/Favorite";
 import IconButton from '@material-ui/core/IconButton';
+import { Button, Modal, } from "react-bootstrap";
 const InfoWindow = (props) => {
-    
+
     console.log(props)
     const populationValues = ["Low", "Medium", "High", "Packed"];
     const qualityValues = ["Poor", "Average", "Great"];
+    const [showGroup, setShowGroup] = useState(false);
+    const [modalEvent, setModalEvent] = useState();
 
+    const [showCreateGroupModal, setShowCreateGroupModal] = useState(false);
+    function handleShowCreateGroupModal() {
+        setShowCreateGroupModal(true)
+    }
+    function handleHideCreateGroupModal() {
+        setShowCreateGroupModal(false)
+    }
     const [fav, setFav] = React.useState(false);
-    function handleFav(){
+    function handleFav() {
         setFav(!fav)
-        if(fav == true){
-          //save it to database
-        }else{
-          //or delete it from database
+        if (fav == true) {
+            //save it to database
+        } else {
+            //or delete it from database
         }
     }
-    
+    function handleGroupClick(event) {
+        setShowGroup(true);
+    }
+    function handleGroupClose() {
+        setShowGroup(false)
+    }
     return (
         <div className="infoWindowStyle">
             <div className='windowHeader'></div>
@@ -26,12 +42,12 @@ const InfoWindow = (props) => {
             <div className='Top-info'>
                 <div className='Park-Info'>
                     <h3>{props.title}{fav &&
-                            <IconButton style={{ color: "#30E98reF" }} onClick={() => { setFav(!fav) }}>
-                                <FavoriteIcon style={{ width: 32, height: 32 }}></FavoriteIcon>
-                            </IconButton>
-                        }
+                        <IconButton style={{ color: "#30E98reF" }} onClick={() => { setFav(!fav) }}>
+                            <FavoriteIcon style={{ width: 32, height: 32 }}></FavoriteIcon>
+                        </IconButton>
+                    }
                         {!fav &&
-                            <IconButton style={{ color: "#30E98F" }}onClick={() => { setFav(!fav) }}>
+                            <IconButton style={{ color: "#30E98F" }} onClick={() => { setFav(!fav) }}>
                                 <Favorite style={{ width: 32, height: 32 }}></Favorite>
                             </IconButton>
                         }</h3>
@@ -44,39 +60,56 @@ const InfoWindow = (props) => {
             <div className='mid-info'>
                 <h5>Average Population: {populationValues[props.population]}</h5>
                 <h5>Quality: {qualityValues[props.quality]}</h5>
-
             </div>
             <div className='bottom-info'>
-                <h5>User Rating</h5>
-                    <div className="rate">
-                        <input type="radio" id="star5" name="rate" value="5" />
-                        <label htmlFor="star5" title="text">5 stars</label>
-                        <input type="radio" id="star4" name="rate" value="4" />
-                        <label htmlFor="star4" title="text">4 stars</label>
-                        <input type="radio" id="star3" name="rate" value="3" />
-                        <label htmlFor="star3" title="text">3 stars</label>
-                        <input type="radio" id="star2" name="rate" value="2" />
-                        <label htmlFor="star2" title="text">2 stars</label>
-                        <input type="radio" id="star1" name="rate" value="1" />
-                        <label htmlFor="star1" title="text">1 star</label>
-                    </div>
-                <div className='quality-box'>
-                    <h5>Park Quality Rating</h5>
-                    <select name="quality-items" id="quality-items">
-                        <option value="Poor">Poor</option>
-                        <option value="Average">Average</option>
-                        <option value="Great">Great</option>
-                    </select>
-                    <select name="population-items" id="population-items">
-                        <option value="Low">Low</option>
-                        <option value="Average">Average</option>
-                        <option value="High">High</option>
-                        <option value="Packed">Packed</option>
-
-                    </select>
-                </div>
-                <br></br>
-                <button className='btn btn-update'>Update</button>
+                <Modal show={showGroup} onHide={handleGroupClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            {props.title}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="review-popup">
+                            <div className="input-fields">
+                                <div className="category-dropBox"><b>Park Quality:</b>
+                                    <select name="quality-items" id="quality-items">
+                                        <option value="Poor">Poor</option>
+                                        <option value="Average">Average</option>
+                                        <option value="Great">Great</option>
+                                    </select>
+                                    <div className="category-dropBox"><b>Park Population:</b></div>
+                                    <select name="population-items" id="population-items">
+                                        <option value="Low">Low</option>
+                                        <option value="Average">Average</option>
+                                        <option value="High">High</option>
+                                        <option value="Packed">Packed</option>
+                                    </select>
+                                    <div className="rate">
+                                        <input type="radio" id="star5" name="rate" value="5" />
+                                        <label htmlFor="star5" title="text">5 stars</label>
+                                        <input type="radio" id="star4" name="rate" value="4" />
+                                        <label htmlFor="star4" title="text">4 stars</label>
+                                        <input type="radio" id="star3" name="rate" value="3" />
+                                        <label htmlFor="star3" title="text">3 stars</label>
+                                        <input type="radio" id="star2" name="rate" value="2" />
+                                        <label htmlFor="star2" title="text">2 stars</label>
+                                        <input type="radio" id="star1" name="rate" value="1" />
+                                        <label htmlFor="star1" title="text">1 star</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer className="d-flex justify-content-between">
+                        <Button variant="primary" onClick={handleGroupClose}>
+                            Confirm Group
+                        </Button>
+                        <Button variant="secondary" onClick={handleGroupClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                <button variant="success" className="btn btn-update" onClick={handleGroupClick}>Leave Review</button>
             </div>
         </div>
     );
